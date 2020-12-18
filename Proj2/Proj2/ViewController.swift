c608568931f26afe4d54b7c0e882128a8c8faefe
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-12-18 13:27:08 +0100

    Lab4 start state

---
+++ Proj2/Proj2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proj2
+//
+//  Created by Magiczne on 18/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
