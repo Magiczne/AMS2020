891f71f71a852b3fcfc17ec8089ca148a7aec298
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-01-15 13:34:03 +0100

    Lab6 init

---
+++ Proj 3/Proj 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proj 3
+//
+//  Created by Magiczne on 15/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
